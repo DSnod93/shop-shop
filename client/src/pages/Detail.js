cb02dd8780544ae63792faf6390ba2e02c3cce6b
DSnoddy <douglassnoddyjr@gmail.com>
2021-05-04 22:06:09 -0800

    update and remove cart items

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,9 +5,17 @@ import { useQuery } from '@apollo/react-hooks';
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif';
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
 
-// function Detail() {
+import Cart from '../components/Cart';
+
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
+
+//  function Detail() {
 //   const { id } = useParams();
 
 //   const [currentProduct, setCurrentProduct] = useState({})
@@ -30,7 +38,7 @@ function Detail() {
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+  const { products, cart } = state;
 
   useEffect(() => {
     if (products.length) {
@@ -42,6 +50,32 @@ function Detail() {
       });
     }
   }, [products, data, dispatch, id]);
+
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 }
+      });
+    }
+  };
+
+
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
+    })
+  }
+
 
   return (
     <>
@@ -61,10 +95,12 @@ function Detail() {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
+            <button onClick={addToCart}>
               Add to Cart
             </button>
-            <button>
+            <button
+              disabled={!cart.find(p => p._id === currentProduct._id)}
+              onClick={removeFromCart}>
               Remove from Cart
             </button>
           </p>
@@ -78,6 +114,7 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
