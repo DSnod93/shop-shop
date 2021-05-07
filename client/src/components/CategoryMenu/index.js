539d7f646de731fe5914beba0beebe299526ba7e
DSnoddy <douglassnoddyjr@gmail.com>
2021-05-06 16:28:09 -0800

    added indexDB to shopping cart

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_CATEGORIES } from "../../utils/queries";
-import { useStoreContext } from "../../utils/GlobalState";
+import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu({ setCategory }) {
   // const { data: categoryData } = useQuery(QUERY_CATEGORIES);
@@ -10,19 +11,31 @@ function CategoryMenu({ setCategory }) {
 
   const [state, dispatch] = useStoreContext();
   const { categories } = state;
-  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
 
   useEffect(() => {
     //if categoryData exists or has changed from the response of useQuery, then run dispatch()
-
     if (categoryData) {
       //execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
       dispatch({
         type: UPDATE_CATEGORIES,
         categories: categoryData.categories
       });
+
+      categoryData.categories.forEach(category => {
+        idbPromise('categories', 'put', category)
+      })
+    } else if (!loading) {
+      idbPromise('categories', 'get').then(categories => {
+        dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories
+        })
+      })
     }
-  }, [categoryData, dispatch]);
+  }, [categoryData, loading, dispatch]);
+
 
   const handleClick = id => {
     dispatch({
@@ -30,7 +43,6 @@ function CategoryMenu({ setCategory }) {
       currentCategory: id
     });
   };
-
   return (
     <div>
       <h2>Choose a Category:</h2>
@@ -38,6 +50,7 @@ function CategoryMenu({ setCategory }) {
         <button
           key={item._id}
           onClick={() => {
+            // setCategory(item._id);
             handleClick(item._id);
           }}
         >
@@ -46,6 +59,6 @@ function CategoryMenu({ setCategory }) {
       ))}
     </div>
   );
-}
+};
 
 export default CategoryMenu;
